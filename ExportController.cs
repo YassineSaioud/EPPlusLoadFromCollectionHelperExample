b1e218efdcc567bb363b377919baf34e5a5e2c33
YassineSaioud <saioud.yassine@gmail.com>
2018-06-15 12:08:55 +0200

    Add controller

---
+++ ExportController.cs
@@ -0,0 +1,23 @@
+﻿using RLD.Extranet.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace RLD.Extranet.Controllers
+{
+    public class ExportController : Controller
+    {
+        public FileContentResult ExcelExportDownload()
+        {
+            var fileInfo = ExportHelper.BuildWorkbookFromCollection("Title",
+                                                                     new Dictionary<string, string>
+                                                                     {
+                                                                         { "Header1", "PropertyName1" },
+                                                                         { "Header1", "PropertyName2" },
+                                                                     },
+                                                                     new List<Object>()
+                                                                     );
+            return File(fileInfo.Contents, "application/vnd.ms-excel", fileInfo.Name);
+        }
+    }
+}
